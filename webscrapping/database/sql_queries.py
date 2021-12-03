11c4ccb8decbe4d2bd22aa745a42ab60389e39d0
Mateus Bessa <mateus_b09@hotmail.com>
2021-12-03 16:01:01 -0300

    Economy documentations and some new dataframe methods

--- webscrapping/database/sql_queries.py
+++ webscrapping/database/sql_queries.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 
 from webscrapping.database.sql_creator import ValorantCreator
@@ -7,6 +8,15 @@ class ValorantQueries:
     def __init__(self):
         self.db = ValorantCreator("valorant")
         self.match_id = 0
+
+        weapon_file = open('..\\matches\\model\\weapon_table.json')
+        self.weapon_data = json.load(weapon_file)
+
+        agent_file = open('..\\matches\\model\\agent_table.json')
+        self.agent_data = json.load(agent_file)
+
+        maps_file = open('..\\matches\\model\\map_table.json')
+        self.maps_data = json.load(maps_file)
 
     def set_match(self, input_match_id: int):
         self.match_id = input_match_id
@@ -61,6 +71,20 @@ class ValorantQueries:
         self.db.cursor.execute(query)
         return self.db.cursor.fetchall()
 
+    def get_full_loadouts(self):
+        query = f"""
+        SELECT Rounds.match_id, RoundEconomies.round_id, RoundEconomies.Round_number, RoundEconomies.Player_id,
+        PlayerMapInstance.team_id, RoundEconomies.remaining_creds, RoundEconomies.loadout_value, RoundEconomies.agent_id,
+        weapon_id, armor_id
+        FROM RoundEconomies
+        INNER JOIN Rounds ON RoundEconomies.round_id = Rounds.round_id
+        INNER JOIN PlayerMapInstance ON PlayerMapInstance.player_id = RoundEconomies.player_id
+        where Match_id = {self.match_id}
+        AND PlayerMapInstance.map_played = {self.match_id}
+        """
+        self.db.cursor.execute(query)
+        return self.db.cursor.fetchall()
+
     def get_events(self):
         query = f"""
             SELECT 'RoundEvents' as ta, RoundEvents.*, 'Rounds' as td, Rounds.*, 'Matches' as te, Matches.*
@@ -93,11 +117,12 @@ class ValorantQueries:
         return self.db.cursor.fetchall()
 
     def get_economy_table(self) -> pd.DataFrame:
-        loadout_query = self.get_loadouts()
-        loadout_df = pd.DataFrame(loadout_query,
-                                  columns=['Match ID', 'Round ID', 'Round', 'Player ID', 'Team ID', 'Remaining Creds',
-                                           'Loadout Value'])
-        loadout_df = loadout_df.sort_values(by=['Round'])
+        """
+        Returns a dataframe of the economy table for the match
+        :return: Round, Team ID, Team Remaining Creds, Team Loadout Value, Team Economy
+         Shield Total Amount, Shield Total Price, Weapon Total Price
+        """
+        loadout_df = self.get_full_loadout_table()
         round_amount = loadout_df["Round"].max()
         split_aux = {i: [] for i in range(1, round_amount + 1)}
         for row in loadout_df.iterrows():
@@ -108,13 +133,18 @@ class ValorantQueries:
         group_aux = {}
         for key, value in split_aux.items():
             raw_group = pd.DataFrame(value)
-            compact = raw_group.groupby(['Team ID']).agg({'Remaining Creds': 'sum', 'Loadout Value': 'sum'})
+            compact = raw_group.groupby(['Team ID']).agg({'Remaining Creds': 'sum', 'Loadout Value': 'sum',
+                                                          'Shield': 'sum', 'Shield Price': 'sum',
+                                                          'Weapon Price': 'sum'})
             compact["Round"] = [key] * 2
             compact["Team ID"] = compact.index
             clean_dict = {"Round": list(compact["Round"]),
                           "Team ID": list(compact["Team ID"]),
                           "Remaining Creds": list(compact["Remaining Creds"]),
-                          "Loadout Value": list(compact["Loadout Value"])}
+                          "Loadout Value": list(compact["Loadout Value"]),
+                          "Shield Amount": list(compact["Shield"]),
+                          "Shield Price": list(compact["Shield Price"]),
+                          "Weapon Price": list(compact["Weapon Price"])}
             compact = pd.DataFrame(clean_dict)
             group_aux[key] = compact
 
@@ -122,11 +152,31 @@ class ValorantQueries:
         final_df["Economy"] = final_df["Remaining Creds"] + final_df["Loadout Value"]
         return final_df
 
-    def get_score_table(self):
+    def get_side_info(self) -> dict:
+        """
+        Returns a dictionary of the side information for team sides
+        :return: team_a: id, team_b: id
+                 attacking_first: id, defending_first: id,
+                 round_df: DataFrame
+        """
         round_df = pd.DataFrame(self.get_all_rounds(), columns=['round_number', 'attacking_team', 'winning_team'])
         teams = tuple(round_df['attacking_team'].unique())
         team_a: int = teams[0]
         team_b: int = teams[1]
+        attacking_first = round_df.iloc[0]['attacking_team']
+        mirror_dict = {team_a: team_b, team_b: team_a}
+        defending_first = mirror_dict[attacking_first]
+        return {"team_a": team_a, "team_b": team_b,
+                "attacking_first": attacking_first, "defending_first": defending_first,
+                "round_df": round_df}
+
+    def get_score_table(self) -> pd.DataFrame:
+        side_info = self.get_side_info()
+        team_a: int = side_info["team_a"]
+        team_b: int = side_info["team_b"]
+        attacking_first = side_info["attacking_first"]
+        defending_first = side_info["defending_first"]
+        round_df = side_info["round_df"]
         team_dict = {team_a: 0, team_b: 0}
         team_scores = {team_a: [], team_b: []}
         for team in round_df['winning_team']:
@@ -148,12 +198,41 @@ class ValorantQueries:
         match_data = self.get_current_match()[0]
         map_name = match_data[4]
         winning_team = match_data[9]
-        round_df = round_df.assign(MapName=map_name, WinningTeam=winning_team)
-        apple = 5 + 1
+        round_df = round_df.assign(MapName=map_name)
+        round_df["score_diff"] = round_df[f"{attacking_first}_scores"] - round_df[f"{defending_first}_scores"]
+        round_df["economy_diff"] = round_df[f"{attacking_first}_economy"] - round_df[f"{defending_first}_economy"]
+        round_df = round_df.assign(MatchWinner=winning_team)
+        return round_df
+
+    def get_full_loadout_table(self):
+        loadouts = self.get_full_loadouts()
+        shield_table = {0: 0, 1: 25, 2: 50}
+        shield_price = {0: 0, 1: 400, 2: 1000}
+        gun_table = self.weapon_data
+        gun_names = {int(key): value["name"] for key, value in gun_table.items()}
+        gun_prices = {int(key): int(value["price"]) for key, value in gun_table.items()}
+        loadout_df = pd.DataFrame(loadouts, columns=['Match ID', 'Round ID', 'Round', 'Player ID', 'Team ID',
+                                                     'Remaining Creds', 'Loadout Value', 'Agent ID', 'Weapon ID',
+                                                     'Armor ID'])
+        loadout_df["Shield"] = loadout_df["Armor ID"].map(shield_table)
+        loadout_df["Shield Price"] = loadout_df["Armor ID"].map(shield_price)
+        loadout_df["Weapon Name"] = loadout_df["Weapon ID"].map(gun_names)
+        loadout_df["Weapon Price"] = loadout_df["Weapon ID"].map(gun_prices)
+        loadout_df["Utility Value"] = \
+            loadout_df["Loadout Value"] - loadout_df["Shield Price"] - loadout_df["Weapon Price"]
+        loadout_df["Utility Value"] = loadout_df["Utility Value"].where(loadout_df["Utility Value"] > 0, 0)
+        return loadout_df
+
+    def generate_current_game_state(self):
+        loadouts = self.get_loadouts()
+        events = self.get_events()
+        loadout_df = pd.DataFrame(loadouts, columns=['Match ID', 'Round ID', 'Round', 'Player ID',
+                                                     'Team ID', 'Remaining Creds', 'Loadout Value'])
+        loadout_df = loadout_df.sort_values(by=['Round'])
+        round_amount = loadout_df["Round"].max()
 
 
 if __name__ == "__main__":
     vq = ValorantQueries()
     vq.set_match(10597)
-    apple = vq.get_current_match()
-    vq.get_score_table()
+    vq.generate_current_game_state()

3c7ced03da4f77e882e44b3a66313b82d8d68d38
Mateus <mateus_b09@hotmail.com>
2022-01-04 22:25:21 -0300

    Fixed round impact API call

--- webscrapping/api/api_wrapper.py
+++ webscrapping/api/api_wrapper.py
@@ -74,17 +74,19 @@ def get_round_impact():
     Json format
     {
         "match_id": 44795,
-        "round": 1
+        "round": 1,
+        "side": "atk"
     }
     """
     input_json = request.get_json(force=True)
     print(input_json)
     match_id = input_json["match_id"]
     round_number = input_json["round"]
+    side = input_json["side"]
     rr.set_match(match_id)
     rr.choose_round(round_number)
-    round_impact_df = rr.get_round_impact_dataframe()
-    round_impact_df["Player"] = round_impact_df.index
+    round_impact_df = rr.get_round_probability(side=side, add_events=True)
+    # round_impact_df["Player"] = round_impact_df.index
     dict_to_return = round_impact_df.to_dict('list')
     return jsonify(dict_to_return)
 

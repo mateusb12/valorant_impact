34ba369f684ac822e8958b2a8df2cbf1afdcc970
mateusb12 <mateus_b09@hotmail.com>
2022-02-17 11:47:33 -0300

    PKL exporter file

---
+++ impact_score/model/lgbm_record.py
@@ -0,0 +1,16 @@
+from impact_score.model.lgbm_model import ValorantLGBM
+
+
+def update_pkl():
+    """
+    Update the pickle file with the new model
+    """
+    vm = ValorantLGBM()
+    vm.setup_dataframe("2000.csv")
+    vm.train_model(optuna_study=True)
+    vm.export_model()
+    vm.show_all_metrics()
+
+
+if __name__ == "__main__":
+    update_pkl()

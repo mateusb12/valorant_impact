ceb88f6fabe301dac31f7329232cb28919b43179
mateusb12 <mateus_b09@hotmail.com>
2022-02-18 17:18:35 -0300

    Created simple aggregation
    Former-commit-id: 9a82aa57e1d1e36ac8cb545b274a6292227fcda3

--- impact_score/rib_rating/rating_aggregation.py
+++ impact_score/rib_rating/rating_aggregation.py
@@ -1,35 +1,29 @@
-from impact_score.json_analyser.api_consumer import get_match_info
 import pandas as pd
 
-
-class RatingAnalyser:
-    def __init__(self, match_id: int):
-        self.match_id = match_id
-        self.data = get_match_info(match_id)
-        self.current_map = self.get_current_map()
-        self.player_data = self.get_player_dict()
-        self.player_names = {player["player"]["id"]: player["player"]["ign"] for player in self.current_map["players"]}
-
-    def get_current_map(self) -> dict:
-        series_by_id = self.data["series"]["seriesById"]
-        matches = series_by_id["matches"]
-        return [match for match in matches if match["id"] == self.match_id][0]
-
-    def get_player_dict(self) -> dict:
-        return {player["player"]["id"]: player for player in self.current_map["players"]}
-
-    def get_player_performance(self):
-        player_stats = self.current_map["stats"]
-        return [(self.player_names[stat["playerId"]],
-                 round(stat["score"] / stat["roundsPlayed"], 2),
-                 round(float(stat["impact"]), 2))
-                for stat in player_stats]
-
-    def export_player_performance(self) -> pd.DataFrame:
-        raw_dict = self.get_player_performance()
-        return pd.DataFrame(raw_dict, columns=["Player", "Score", "Impact"])
+from impact_score.model.time_analyser import time_metrics
+from impact_score.rib_rating.rating_single_match import RatingAnalyser
+from timeit import default_timer as timer
 
 
-if __name__ == "__main__":
-    ra = RatingAnalyser(55500)
-    print(ra.export_player_performance())
+class RatingAggregator:
+    def __init__(self):
+        self.raw_id_list = pd.read_csv("id_list.csv")
+        self.ra = RatingAnalyser()
+        self.ids = self.raw_id_list["Match Id"].tolist()
+
+    def aggregate(self) -> pd.DataFrame:
+        aggregate_pot = []
+        start = timer()
+        size = len(self.ids)
+        for index, match_id in enumerate(self.ids):
+            loop = timer()
+            time_metrics(start=start, end=loop, index=index, size=size, tag="match", element=match_id)
+            self.ra.set_match(match_id)
+            aggregate_pot.append(self.ra.export_player_performance())
+        return pd.concat(aggregate_pot)
+
+
+if __name__ == '__main__':
+    ra = RatingAggregator()
+    champions = ra.aggregate()
+    print(champions)

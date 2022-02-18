135234563b645824158cc4ef4623ca0797090e58
mateusb12 <mateus_b09@hotmail.com>
2022-02-18 17:38:25 -0300

    Bug on aggregation
    Former-commit-id: 5886af95c552f0caf0156f90168e1f4024e4e790

--- impact_score/rib_rating/rating_single_match.py
+++ impact_score/rib_rating/rating_single_match.py
@@ -28,12 +28,16 @@ class RatingAnalyser:
     def export_player_performance(self) -> pd.DataFrame:
         player_stats = self.current_map["stats"]
         raw_list = [(self.player_names[stat["playerId"]],
-                     round(stat["score"] / stat["roundsPlayed"], 2),
-                     round(float(stat["impact"]), 2))
+                     round(float(stat["impact"]), 2),
+                     round(stat["score"] / stat["roundsPlayed"], 2))
                     for stat in player_stats]
-        return pd.DataFrame(raw_list, columns=["Player", "Score", "Impact"])
+        pre_df = pd.DataFrame(raw_list, columns=["Player", "Impact", "Score"])
+        df_types = pre_df.dtypes
+        return pre_df
 
 
 if __name__ == "__main__":
     ra = RatingAnalyser()
-    print(ra.export_player_performance())
+    ra.set_match(54900)
+    ex = ra.export_player_performance()
+    print(ex)

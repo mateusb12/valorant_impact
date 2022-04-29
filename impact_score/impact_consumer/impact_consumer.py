3a58158716521fdf1606d5da2ce006e5feb93587
mateu <mateus_b09@hotmail.com>
2022-04-30 00:10:42 +0200

    Documentation for impact_consumer.py

--- impact_score/impact_consumer/impact_consumer.py
+++ impact_score/impact_consumer/impact_consumer.py
@@ -1,3 +1,5 @@
+from typing import Union, Dict, Any
+
 import requests
 
 # from impact_score.impact.match_analysis import RoundReplay
@@ -5,16 +7,30 @@ from impact_score.json_analyser.analyse_json import Analyser
 
 
 def export_impact(match_id: int, input_analyser: Analyser) -> dict:
+    """
+    Export the impact of each player in a match.
+    :param match_id: The match id. (60206)
+    :param input_analyser: Analyser object instance.
+    :return: Dictionary containing event details. For example:
+    {
+        "round_1": [
+            {
+                "author": "Crashies",
+                "victim": "Nivera",
+                "event": "kill",
+                "impact": "0.039",
+                "probability_before": "0.2318",
+                "probability_after": "0.2709"
+                "timing": "26185",
+    """
     analyser = input_analyser
     analyser.set_match(match_id)
     analyser.set_config(round=1)
-    # rr_object = RoundReplay()
     details = analyser.export_player_details()
     max_round = analyser.round_amount
     match_impact_dict = {f"Round_{i}": [] for i in range(1, max_round + 1)}
     for item in range(1, max_round + 1):
         analyser.set_config(round=item)
-        # probability_table = rr_object.get_round_probability(side="atk")
         for event in analyser.round_events:
             author_id = event["author"]
             victim_id = event["victim"]
@@ -31,7 +47,21 @@ def export_impact(match_id: int, input_analyser: Analyser) -> dict:
     return match_impact_dict
 
 
-def export_players_impact(match_id: int, input_analyser: Analyser) -> dict:
+def export_players_impact(match_id: int, input_analyser: Analyser, **kwargs) -> Union[int, dict[Any, int]]:
+    """
+    Export the impact of each player in a match.
+    :param match_id: The match id. (60206)
+    :param input_analyser: Analyser object instance.
+    :param kwargs: player_name="crashies"
+    :return: Dictionary with impact for each player, unless kwargs player_name is set. Example:
+    {
+        "keznit": 0.9838,
+        "Nivera": 1.141,
+        "Jamppi": 3.176,
+        "Mazino": 1.852,
+        "ScreaM": 0.988
+    }
+    """
     match_data = export_impact(match_id, input_analyser)
     player_impact_dict = {}
 
@@ -44,9 +74,13 @@ def export_players_impact(match_id: int, input_analyser: Analyser) -> dict:
                 impact = float(event['impact'])
                 player_impact_dict[author] += impact
 
-    return player_impact_dict
+    if "player_name" in kwargs:
+        return player_impact_dict[kwargs["player_name"]]
+    else:
+        return player_impact_dict
 
 
 if __name__ == "__main__":
     test = export_impact(match_id=60206, input_analyser=Analyser())
+    test2 = export_players_impact(match_id=60206, input_analyser=Analyser())
     print(test)

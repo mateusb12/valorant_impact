64653b8290f60e67a011f18a5febbac9470f26fc
mateu <mateus_b09@hotmail.com>
2022-05-31 02:23:16 +0200

    Some features were fixed
    Former-commit-id: 149d96c042bf09d858bfa5f73d005cb6bde3c1de

--- impact_score/model/dataset_preparation/dataset_prep.py
+++ impact_score/model/dataset_preparation/dataset_prep.py
@@ -6,10 +6,11 @@ import pandas as pd
 def prepare_dataset(filename: str = "4000.csv") -> pd.DataFrame:
     dataset_folder = Path(datasets_reference(), filename)
     index_features = ["RoundID", "MatchID", "RoundNumber", "Team_A_ID", "Team_A_Name", "Team_B_ID", "Team_B_Name",
-                      "MapName"]
+                      "MapName", "RoundTime"]
     map_features = [f"MapName_{item}" for item in ["Ascent", "Bind", "Breeze", "Fracture", "Haven", "Icebox", "Split"]]
-    redundant_features_atk = [f"ATK_{item}" for item in ["weaponValue", "shields", "remainingCreds"]]
-    redundant_features_def = [f"DEF_{item}" for item in ["weaponValue", "shields", "remainingCreds"]]
+    redundant_features = ["weaponValue", "shields", "remainingCreds", "loadoutValue"]
+    redundant_features_atk = [f"ATK_{item}" for item in redundant_features]
+    redundant_features_def = [f"DEF_{item}" for item in redundant_features]
     useless_features = index_features + map_features + redundant_features_atk + redundant_features_def
     df = pd.read_csv(dataset_folder)
     df = df.drop(useless_features, axis=1)

e27019a68c3b1eb5ab2b8ad011b029294249cec0
mateu <mateus_b09@hotmail.com>
2022-06-21 17:07:32 +0200

    Refactored gamestate

--- impact_score/json_analyser/core/analyser_gamestate.py
+++ impact_score/json_analyser/core/analyser_gamestate.py
@@ -1,5 +1,6 @@
 from impact_score.json_analyser.pool.analyser_pool import CoreAnalyser, analyser_pool
 from impact_score.json_analyser.core.analyser_tools import AnalyserTools
+from impact_score.json_analyser.wrap.analyser_loader import get_analyser
 
 
 class AnalyserGamestate:
@@ -8,19 +9,31 @@ class AnalyserGamestate:
         self.tools = AnalyserTools(input_core_analyser)
         self.current_round_sides = self.tools.get_current_sides()
         self.round_table = self.tools.get_round_table()
+        self.shield_table = {0: 0, 1: 25, 2: 50}
 
-    def create_player_gamestate_dict(self, input_value: dict, input_shield_table: dict):
-        weapon_id = str(input_value["weaponId"])
-        weapon_price = int(self.a.weapon_data[weapon_id]["price"]) if weapon_id != "None" else 0
-        agent_id = str(input_value["agentId"])
-        shield_id = str(input_value["shieldId"])
-        shield_value = input_shield_table[int(shield_id)] if shield_id != "None" else 0
-        agent_role = self.a.agent_data[agent_id]["role"]
-        team_number = input_value["name"]["team_number"]
-        team_side = self.current_round_sides[team_number]
-        cont_dict = {"loadoutValue": input_value["loadoutValue"], "weaponValue": weapon_price,
-                     "remainingCreds": input_value["remainingCreds"], "operators": 1 if weapon_id == "15" else 0,
-                     "shields": shield_value, agent_role: 1}
+    def get_weapon_price(self, weapon_id: int):
+        new_id = str(weapon_id)
+        return int(self.a.weapon_data[new_id]["price"]) if new_id != "None" else 0
+
+    def get_agent_role(self, agent_id: int):
+        new_id = str(agent_id)
+        return self.a.agent_data[new_id]["role"]
+
+    def get_shield_value(self, shield_id: int):
+        new_id = str(shield_id)
+        return self.shield_table[int(new_id)] if new_id != "None" else 0
+
+    def get_team_side(self, team_number: int):
+        return self.current_round_sides[team_number]
+
+    def get_player_gamestate_dict(self, player_dict: dict):
+        weapon_price = self.get_weapon_price(player_dict["weaponId"])
+        agent_role = self.get_agent_role(player_dict["agentId"])
+        shield_value = self.get_shield_value(player_dict["shieldId"])
+        return {"loadoutValue": player_dict["loadoutValue"], "weaponValue": weapon_price,
+                "remainingCreds": player_dict["remainingCreds"],
+                "operators": 1 if player_dict["weaponId"] == "15" else 0,
+                "shields": shield_value, agent_role: 1}
 
     def generate_single_event_values(self, **kwargs):
         player_table: dict = self.a.current_status
@@ -29,22 +42,11 @@ class AnalyserGamestate:
         features = team_variables + roles
         atk_dict = {item: 0 for item in features}
         def_dict = {item: 0 for item in features}
-        shield_table = {0: 0, 1: 25, 2: 50}
 
         for value in player_table.values():
             if player_state := value["alive"]:
-                weapon_id = str(value["weaponId"])
-                weapon_price = int(self.a.weapon_data[weapon_id]["price"]) if weapon_id != "None" else 0
-                agent_id = str(value["agentId"])
-                shield_id = str(value["shieldId"])
-                shield_value = shield_table[int(shield_id)] if shield_id != "None" else 0
-                agent_role = self.a.agent_data[agent_id]["role"]
-                team_number = value["name"]["team_number"]
-                round_sides = self.current_round_sides
-                team_side = self.current_round_sides[team_number]
-                cont_dict = {"loadoutValue": value["loadoutValue"], "weaponValue": weapon_price,
-                             "remainingCreds": value["remainingCreds"], "operators": 1 if weapon_id == "15" else 0,
-                             "shields": shield_value, agent_role: 1}
+                team_side = self.get_team_side(value["name"]["team_number"])
+                cont_dict = self.get_player_gamestate_dict(value)
                 for feature, feature_value in cont_dict.items():
                     if team_side == "attacking":
                         atk_dict[feature] += feature_value
@@ -65,9 +67,9 @@ class AnalyserGamestate:
 
 
 def __main():
-    a = analyser_pool.acquire()
-    a.set_match(68821)
+    a = get_analyser(68821)
     ag = AnalyserGamestate(a)
+    ag.generate_single_event_values(timestamp=0, winner=1, plant=91990)
 
 
 if __name__ == "__main__":

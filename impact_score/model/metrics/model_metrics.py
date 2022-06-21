0bc752b432e1a6929df2478cbe09b9ee3a2cc3eb
mateu <mateus_b09@hotmail.com>
2022-06-21 18:04:51 +0200

    More model metrics
    Former-commit-id: b31c26b146964cf7f41dfba153c1175982c1310a

--- impact_score/model/metrics/model_metrics.py
+++ impact_score/model/metrics/model_metrics.py
@@ -1,7 +1,8 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
-from sklearn.metrics import log_loss, brier_score_loss, confusion_matrix, classification_report
+from sklearn.metrics import log_loss, brier_score_loss, confusion_matrix, classification_report, roc_curve, \
+    roc_auc_score
 from sklearn.model_selection import cross_val_score
 from termcolor import colored
 
@@ -10,6 +11,7 @@ from impact_score.model.lgbm_model import ValorantLGBM, get_trained_model, get_p
 
 class ModelMetrics:
     def __init__(self, input_lgbm: ValorantLGBM):
+        self.model_obj = input_lgbm
         self.model = input_lgbm.model
         self.X = input_lgbm.X
         self.X_train = input_lgbm.X_train
@@ -17,6 +19,7 @@ class ModelMetrics:
         self.Y_train = input_lgbm.Y_train
         self.Y_test = input_lgbm.Y_test
         self.pred_proba, self.pred_proba_test = [None] * 2
+        self.y_score = self.get_y_score()
 
     def get_brier_score(self) -> float:
         y_true = self.Y_test
@@ -85,12 +88,41 @@ class ModelMetrics:
         f1 = classification_report(self.Y_test, Y_pred, output_dict=True)["weighted avg"]["f1-score"]
         print(f"F1 score → {f1}")
 
+    def get_y_score(self):
+        return self.model_obj.model.predict_proba(self.model_obj.X_test)[:, 1]
+
+    def get_tp_fp_rates(self) -> dict:
+        false_positive_rate, true_positive_rate, threshold = roc_curve(self.model_obj.Y_test, self.y_score)
+        return {"false_positive_rate": false_positive_rate, "true_positive_rate": true_positive_rate,
+                "threshold": threshold}
+
+    def get_roc_auc_score(self) -> float:
+        y_score = self.y_score
+        score = roc_auc_score(self.model_obj.Y_test, y_score)
+        print('roc_auc_score for LightGBM: ', score)
+        return score
+
+    def plot_roc_curve(self):
+        auc_dict = self.get_tp_fp_rates()
+        plt.subplots(1, figsize=(10, 7))
+        plt.title('Receiver Operating Characteristic - LightGBM')
+        false_positive_rate = auc_dict["false_positive_rate"]
+        true_positive_rate = auc_dict["true_positive_rate"]
+        plt.plot(false_positive_rate, true_positive_rate)
+        plt.plot([0, 1], ls="--")
+        plt.plot([0, 0], [1, 0], c=".7"), plt.plot([1, 1], c=".7")
+        plt.ylabel('True Positive Rate')
+        plt.xlabel('False Positive Rate')
+        plt.show()
+
     def show_all(self):
         self.get_feature_importance()
         self.get_model_precision()
         self.get_brier_score()
         self.get_confusion_matrix()
         self.get_f1_score()
+        self.get_roc_auc_score()
+        self.plot_roc_curve()
 
 
 def __main():

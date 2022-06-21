34173a8adde08b85a32bad3eb05ea734de8d133d
mateu <mateus_b09@hotmail.com>
2022-06-21 13:10:15 +0200

    Finished analyser refactoring

--- impact_score/json_analyser/analyser_gamestate.py
+++ impact_score/json_analyser/analyser_gamestate.py
@@ -1,30 +1,20 @@
-from impact_score.json_analyser.analyse_json import get_map_dict, create_player_table, get_round_events
-from impact_score.json_analyser.analyser_file_loader import get_agent_data, get_weapon_data
+from impact_score.json_analyser.analyser_pool import CoreAnalyser, analyser_pool
+from impact_score.json_analyser.analyser_tools import AnalyserTools
 
 
 class AnalyserGamestate:
-    def __init__(self, json_data: dict, input_match_id: int):
-        self.data = json_data
-        self.chosen_round = 1
-        self.map_dict = get_map_dict(json_data, input_match_id)
-        self.attacking_first_team: int = self.map_dict["attackingFirstTeamNumber"]
-        self.defending_first_team: int = 1 if self.attacking_first_team == 2 else 2
-        self.current_status = create_player_table(json_data, self.map_dict)
-        self.round_events = None
-        self.agent_data = get_agent_data()
-        self.weapon_data = get_weapon_data()
-
-    def choose_round(self, desired_round: int):
-        self.chosen_round = desired_round
-        self.round_events = get_round_events(self.data, self.chosen_round)
+    def __init__(self, input_core_analyser: CoreAnalyser):
+        self.a = input_core_analyser
+        self.tools = AnalyserTools(input_core_analyser)
+        self.current_round_sides = self.tools.get_current_sides()
 
     def create_player_gamestate_dict(self, input_value: dict, input_shield_table: dict):
         weapon_id = str(input_value["weaponId"])
-        weapon_price = int(self.weapon_data[weapon_id]["price"]) if weapon_id != "None" else 0
+        weapon_price = int(self.a.weapon_data[weapon_id]["price"]) if weapon_id != "None" else 0
         agent_id = str(input_value["agentId"])
         shield_id = str(input_value["shieldId"])
         shield_value = input_shield_table[int(shield_id)] if shield_id != "None" else 0
-        agent_role = self.agent_data[agent_id]["role"]
+        agent_role = self.a.agent_data[agent_id]["role"]
         team_number = input_value["name"]["team_number"]
         team_side = self.current_round_sides[team_number]
         cont_dict = {"loadoutValue": input_value["loadoutValue"], "weaponValue": weapon_price,
@@ -32,7 +22,7 @@ class AnalyserGamestate:
                      "shields": shield_value, agent_role: 1}
 
     def generate_single_event_values(self, **kwargs):
-        player_table: dict = self.current_status
+        player_table: dict = self.a.current_status
         team_variables = ["loadoutValue", "weaponValue", "shields", "remainingCreds", "operators", "kills"]
         roles = ["Initiator", "Duelist", "Sentinel", "Controller"]
         features = team_variables + roles
@@ -43,11 +33,11 @@ class AnalyserGamestate:
         for value in player_table.values():
             if player_state := value["alive"]:
                 weapon_id = str(value["weaponId"])
-                weapon_price = int(self.weapon_data[weapon_id]["price"]) if weapon_id != "None" else 0
+                weapon_price = int(self.a.weapon_data[weapon_id]["price"]) if weapon_id != "None" else 0
                 agent_id = str(value["agentId"])
                 shield_id = str(value["shieldId"])
                 shield_value = shield_table[int(shield_id)] if shield_id != "None" else 0
-                agent_role = self.agent_data[agent_id]["role"]
+                agent_role = self.a.agent_data[agent_id]["role"]
                 team_number = value["name"]["team_number"]
                 team_side = self.current_round_sides[team_number]
                 cont_dict = {"loadoutValue": value["loadoutValue"], "weaponValue": weapon_price,
@@ -60,13 +50,23 @@ class AnalyserGamestate:
                         def_dict[feature] += feature_value
 
         round_time = kwargs["timestamp"]
-        regular_time, spike_time = self.generate_spike_timings(kwargs["timestamp"], kwargs["plant"])
+        regular_time, spike_time = self.tools.generate_spike_timings(kwargs["timestamp"], kwargs["plant"])
         round_winner = kwargs["winner"] if "winner" in kwargs else None
-        final_dict = {"RegularTime": regular_time, "SpikeTime": spike_time, "MapName": self.map_name["name"],
-                      "FinalWinner": round_winner, "RoundID": self.chosen_round, "MatchID": self.match_id,
-                      "RoundNumber": self.round_number, "RoundTime": round_time}
+        final_dict = {"RegularTime": regular_time, "SpikeTime": spike_time, "MapName": self.a.map_name,
+                      "FinalWinner": round_winner, "RoundID": self.a.chosen_round, "MatchID": self.a.match_id,
+                      "RoundNumber": self.a.round_number, "RoundTime": round_time}
         for key, value in atk_dict.items():
             final_dict[f"ATK_{key}"] = value
         for key, value in def_dict.items():
             final_dict[f"DEF_{key}"] = value
         return final_dict
+
+
+def __main():
+    a = analyser_pool.acquire()
+    a.set_match(68821)
+    ag = AnalyserGamestate(a)
+
+
+if __name__ == "__main__":
+    __main()
---
+++ impact_score/json_analyser/analyser_tools.py
@@ -0,0 +1,82 @@
+from typing import Tuple
+
+from impact_score.json_analyser.analyser_pool import CoreAnalyser, analyser_pool
+
+
+class AnalyserTools:
+    def __init__(self, input_core_analyser: CoreAnalyser):
+        self.a = input_core_analyser
+
+    def get_plant_timestamp(self) -> float or None:
+        return next((h["timing"] for h in self.a.round_events if h["event"] == "plant"), None)
+
+    def get_round_winner(self) -> int:
+        for r in self.a.map_dict["rounds"]:
+            if r["number"] == self.a.chosen_round:
+                return 1 if r["winningTeamNumber"] == self.a.attacking_first_team else 0
+
+    def are_sides_swapped(self) -> bool:
+        if 1 <= self.a.chosen_round >= 12:
+            return False
+        elif 13 <= self.a.chosen_round <= 24:
+            return True
+        else:
+            remaining = self.a.chosen_round - 24
+            return remaining % 2 == 0
+
+    def get_current_sides(self):
+        if swap := self.are_sides_swapped():
+            return {self.a.attacking_first_team: "defending", self.a.defending_first_team: "attacking"}
+        else:
+            return {self.a.attacking_first_team: "attacking", self.a.defending_first_team: "defending"}
+
+    def get_player_sides(self) -> dict:
+        team_sides = self.get_current_sides()
+        return {player: team_sides[value["team_number"]] for player, value in self.a.current_status.items()}
+
+    def generate_spike_timings(self, round_millis: int, plant_millis: int) -> Tuple:
+        if round_millis >= 100000 or self.a.defuse_happened:
+            regular_time = 0
+            spike_time = 0
+        elif round_millis == plant_millis:
+            regular_time = 0
+            spike_time = 45000
+        elif (
+                plant_millis is not None
+                and round_millis <= plant_millis
+                or plant_millis is None
+        ):
+            regular_time = 100000 - round_millis
+            spike_time = 0
+        else:
+            regular_time = 0
+            spike_time = 45000 - (round_millis - plant_millis)
+
+        def round_func(x):
+            return int(round(x / 1000))
+
+        regular_time = round_func(regular_time)
+        spike_time = round_func(spike_time)
+        return regular_time, spike_time
+
+    def get_team_a(self) -> dict:
+        """
+        Get the team A of the current map
+        :return: {'id': 3975, 'name': 'Pioneers'}
+        """
+        aux = self.a.data["series"]["seriesById"]
+        return {"id": aux["team1"]["id"], "name": aux["team1"]["name"]}
+
+    def get_team_b(self) -> dict:
+        """
+        Get the team B of the current map
+        :return: {'id': 588, 'name': 'Knights'}
+        """
+        aux = self.a.data["series"]["seriesById"]
+        return {"id": aux["team2"]["id"], "name": aux["team2"]["name"]}
+
+
+def __main():
+    a = analyser_pool.acquire()
+    a.set_match(68821)
+    aw = AnalyserTools(a)

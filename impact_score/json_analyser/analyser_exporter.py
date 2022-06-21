d4082699b20be1712a85b3854ee215912cbddc43
mateu <mateus_b09@hotmail.com>
2022-06-21 12:28:02 +0200

    Refactored analyser_exporter with composition instead of inheritance
    Former-commit-id: f99a5911629d844f90465c7385b027a36b0260cd

--- impact_score/json_analyser/analyser_exporter.py
+++ impact_score/json_analyser/analyser_exporter.py
@@ -1,22 +1,12 @@
-from impact_score.json_analyser.analyse_json import Analyser, get_map_dict, create_player_table
-from impact_score.json_analyser.analyser_file_loader import get_agent_data, get_weapon_data
-from impact_score.json_analyser.analyser_pool import analyser_pool
+from impact_score.json_analyser.analyser_pool import analyser_pool, CoreAnalyser
 
 
 class AnalyserExporter:
-    def __init__(self, data: dict, match_id: int):
-        self.data = data
-        self.match_id = match_id
-        self.map_dict = get_map_dict(data, match_id)
-        self.attacking_first_team: int = self.map_dict["attackingFirstTeamNumber"]
-        self.defending_first_team: int = 1 if self.attacking_first_team == 2 else 2
-        self.current_status = create_player_table(data, self.map_dict)
-        self.round_events = None
-        self.agent_data = get_agent_data()
-        self.weapon_data = get_weapon_data()
+    def __init__(self, input_core_analyser: CoreAnalyser):
+        self.a = input_core_analyser
 
     def export_round_events(self) -> dict:
-        events = self.data["matches"]["matchDetails"]["events"]
+        events = self.a.data["matches"]["matchDetails"]["events"]
 
         for event in events:
             killer_id = event["playerId"]
@@ -24,21 +14,21 @@ class AnalyserExporter:
             if killer_id is None and victim_id is None:
                 killer_name, killer_agent_id, killer_agent_name = None, None, None
             else:
-                killer_name = self.current_status[killer_id]["name"]["ign"]
-                killer_agent_id = self.current_status[killer_id]["agentId"]
-                killer_agent_name = self.agent_data[str(killer_agent_id)]["name"]
+                killer_name = self.a.current_status[killer_id]["name"]["ign"]
+                killer_agent_id = self.a.current_status[killer_id]["agentId"]
+                killer_agent_name = self.a.agent_data[str(killer_agent_id)]["name"]
 
             if event["eventType"] in ["kill", "revival"]:
-                victim_name = self.current_status[victim_id]["name"]["ign"]
-                victim_agent_id = self.current_status[victim_id]["agentId"]
-                victim_agent_name = self.agent_data[str(victim_agent_id)]["name"]
+                victim_name = self.a.current_status[victim_id]["name"]["ign"]
+                victim_agent_id = self.a.current_status[victim_id]["agentId"]
+                victim_agent_name = self.a.agent_data[str(victim_agent_id)]["name"]
                 event["victim_agent_name"] = victim_agent_name
                 event["victim_name"] = victim_name
             else:
                 event["victim_agent_name"] = "None"
                 event["victim_name"] = "None"
             if event["weaponId"] is not None:
-                weapon = self.weapon_data[str(event["weaponId"])]
+                weapon = self.a.weapon_data[str(event["weaponId"])]
             else:
                 weapon = {"weaponId": "None", "name": event["ability"]}
             event["killer_name"] = killer_name
@@ -48,32 +38,31 @@ class AnalyserExporter:
         return events
 
     def export_player_agent_picks(self) -> dict:
-        map_dict = self.map_dict
+        map_dict = self.a.map_dict
         agent_pick_dict = {}
         for item in map_dict["players"]:
             player_name = item["player"]["ign"]
             agent_id = item["agentId"]
-            agent_name = self.agent_data[str(agent_id)]["name"]
+            agent_name = self.a.agent_data[str(agent_id)]["name"]
             agent_pick_dict[player_name] = agent_name
         return agent_pick_dict
 
     def export_player_details(self) -> dict:
-        map_dict = self.map_dict
+        map_dict = self.a.map_dict
         details_dict = {}
         for item in map_dict["players"]:
             player_name = item["player"]["ign"]
             agent_id = item["agentId"]
-            agent_name = self.agent_data[str(agent_id)]["name"]
+            agent_name = self.a.agent_data[str(agent_id)]["name"]
             player_id = item["playerId"]
             details_dict[player_id] = {"agent_name": agent_name, "player_name": player_name}
         return details_dict
 
 
 def __main():
-    ap = analyser_pool
-    a = ap.acquire()
+    a = analyser_pool.acquire()
     a.set_match(65588)
-    ae = AnalyserExporter(a.data, a.match_id)
+    ae = AnalyserExporter(a)
     a = ae.export_round_events()
     b = ae.export_player_agent_picks()
     c = ae.export_player_details()
--- impact_score/json_analyser/analyser_wrapper.py
+++ impact_score/json_analyser/analyser_wrapper.py
@@ -1,16 +1,18 @@
 import pandas as pd
 
+from impact_score.json_analyser.analyser_pool import analyser_pool, CoreAnalyser
+
 
 class AnalyserWrapper:
-    def __init__(self, analyser):
-        self.analyser = analyser
+    def __init__(self, input_core_analyser: CoreAnalyser):
+        self.a = input_core_analyser
 
     def generate_map_metrics(self) -> list:
         """
         Generates the dataframe body. See get_feature_labels().
         """
         map_events = []
-        for i in range(1, self.round_amount + 1):
+        for i in range(1, self.a.round_amount + 1):
             self.set_config(round=i)
             map_events += self.generate_full_round()
         return map_events
@@ -27,7 +29,9 @@ class AnalyserWrapper:
 
 
 def __main():
-    pass
+    a = analyser_pool.acquire()
+    a.set_match(65588)
+    aw = AnalyserWrapper(a)
 
 
 if __name__ == "__main__":

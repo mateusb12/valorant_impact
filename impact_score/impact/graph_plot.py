6bfdecbdd43fa17dbb46ac6fdaa2b339e92c0ccc
mateusb12 <mateus_b09@hotmail.com>
2022-07-17 15:04:38 -0300

    Plot round color improvements
    Former-commit-id: 8399c25a82bd45465ed46631b6912b5fb84d0e5b

--- impact_score/impact/graph_plot.py
+++ impact_score/impact/graph_plot.py
@@ -1,3 +1,5 @@
+from line_profiler_pycharm import profile
+
 from impact_score.impact.match_analysis import RoundReplay
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -39,7 +41,6 @@ class GraphPlotter:
         for column in integer_columns:
             kills_data_df[column] = kills_data_df[column].astype(int)
         kills_data_df["abilityName"] = kills_data_df["ability"].map(self.ability_data)
-        # Create a column called means, which has weapon_name or AbilityName if its not 0
         zip_means = zip(kills_data_df["weapon_name"], kills_data_df["abilityName"])
         means_pot = []
         for weapon, ability in zip_means:
@@ -64,9 +65,9 @@ class GraphPlotter:
         size = len(kills_data_df)
         kills_data_df["stamp"] = [chr(i) for i in range(65, 65 + size)]
         color_mapping = {"start": "green",
-                         "kill": "yellow",
-                         "plant": "purple",
-                         "defuse": "purple",
+                         "kill": "lightseagreen",
+                         "plant": "mediumvioletred",
+                         "defuse": "mediumvioletred",
                          "revival": "brown"}
         kills_data_df["color"] = kills_data_df["event"].map(color_mapping)
         kills_data_df["tag"] = tag_pot
@@ -78,48 +79,63 @@ class GraphPlotter:
         duplicated_stamps = [int(ele) for index, ele in enumerate(single_stamps) for i in range(2)]
         color_list = input_data_df["color"].tolist()
         tag_list = input_data_df["tag"].tolist()
+        stamp_list = input_data_df["stamp"].tolist()
         for index in range(len(color_list)):
             color_list.insert(index * 2, None)
             tag_list.insert(index * 2, None)
+            stamp_list.insert(index * 2, None)
+        full_tag = [f"{stamp} → {tag}" for stamp, tag in zip(stamp_list, tag_list)]
         la = input_data_df["probability_before"].tolist()
         lb = input_data_df["probability_after"].tolist()
         interpolated_probs = []
         for index in range(len(la)):
             interpolated_probs.extend((float(la[index]), float(lb[index])))
         return {"Round time": duplicated_stamps,
-                "Win_probability": [100*round(elem, 2) for elem in interpolated_probs],
+                "Win_probability": [100 * round(elem, 2) for elem in interpolated_probs],
                 "Colors": color_list,
-                "Tags": tag_list}
+                "Tags": full_tag,
+                "Stamps": stamp_list}
 
     def setup_graph_data(self):
         self.kills_data_df = self.get_kill_dataframe()
         self.plot_data = self.get_probability_points(self.kills_data_df)
+
+    @staticmethod
+    def get_color_pattern(chosen_side: str) -> tuple[str, str]:
+        line_color_dict = {"atk": "red", "def": "blue"}
+        title_dict = {"atk": "Attack", "def": "Defense"}
+        return line_color_dict[chosen_side], title_dict[chosen_side]
 
     def plot_round(self, **kwargs):
         desired_round_number = kwargs.get("round_number", None)
         self.chose_round(desired_round_number)
         self.setup_graph_data()
         chosen_side = "atk"
-        color_dict = {"atk": "red", "def": "blue"}
-        title_dict = {"atk": "Attack", "def": "Defense"}
 
         fig = plt.figure(figsize=(14, 7))
         fig.tight_layout()
         plt.rcParams.update({'font.size': 13})
 
+        line_color, title = self.get_color_pattern(chosen_side)
+        grid_color = "black"
+
         plt.plot(self.plot_data["Round time"], self.plot_data["Win_probability"],
-                 color=color_dict[chosen_side], zorder=0)
+                 color=line_color, zorder=0)
+
         self.ax = plt.gca()
+        # self.ax.set_facecolor('grey')
+        self.ax.set_facecolor("#dbd9d9a6")
         self.ax.spines["left"].set_position(("data", -2))
         self.ax.set_xlabel('Round time (s)', rotation=0, labelpad=10)
         self.ax.set_ylabel('Win probability (%)', rotation=90, labelpad=20)
-        self.ax.grid(b=True, which="major", color="orange", linestyle="-", zorder=0, linewidth=.1, alpha=0.5)
+        self.ax.grid(b=True, which="major", color=grid_color,
+                     linestyle="-", zorder=0, linewidth=.2, alpha=.75)
 
         self.apply_time_formatter()
         self.apply_markers()
         self.plot_round_events()
 
-        plt.title(f"{title_dict[chosen_side]} win probability over time")
+        plt.title(f"{title} win probability over time")
         plt.show()
 
     def apply_time_formatter(self):
@@ -130,9 +146,16 @@ class GraphPlotter:
         x_data = self.plot_data["Round time"]
         y_data = self.plot_data["Win_probability"]
         color_data = self.plot_data["Colors"]
-        for round_time, win_probability, color in zip(x_data, y_data, color_data):
+        stamp_data = self.plot_data["Stamps"]
+        y_offset = .82
+        x_offset = -.3
+        for round_time, win_probability, color, stamp in zip(x_data, y_data, color_data, stamp_data):
             if color is not None:
-                self.ax.scatter(x=round_time, y=win_probability, color=color, edgecolor="w", marker="o", s=70, zorder=2)
+                self.ax.scatter(x=round_time, y=win_probability, color=color,
+                                edgecolor="white", marker="o", s=70, zorder=2, linewidth=3)
+                coords = (round_time-x_offset, win_probability+y_offset)
+                self.ax.annotate(text=stamp, xy=(round_time, win_probability),
+                                 xytext=coords)
 
     def plot_round_events(self):
         x_data = self.plot_data["Round time"]
@@ -149,5 +172,4 @@ class GraphPlotter:
 if __name__ == "__main__":
     match_id = 60206
     gp = GraphPlotter(match_id)
-    gp.setup_graph_data()
-    gp.plot_round()
+    gp.plot_round(round_number=3)

20db93d6d2fc292abfc43ea5526f6da594911213
mateusb12 <mateus_b09@hotmail.com>
2022-07-24 20:06:37 -0300

    Dataset generator now has a persistent data pipeline
    Former-commit-id: 50def29c3f4954de50f27739c55c41ac34235245

--- impact_score/model/progress_printer.py
+++ impact_score/model/progress_printer.py
@@ -9,6 +9,30 @@ def convert_seconds_to_time(seconds: int) -> str:
     return f"{raw_list[0]}h {raw_list[1]}m {raw_list[2]}s"
 
 
+def print_progress_bar(iteration: int, total: int, prefix='', suffix='', decimals: int = 1, length: int = 100,
+                       fill='█', print_end="\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    return fill * filledLength + '-' * (length - filledLength)
+    # full_string = f'\r{prefix} |{bar}| {percent}% {suffix}'
+    # print(colored(full_string, "yellow"), end=print_end)
+    # # Print New Line on Complete
+    # if iteration == total:
+    #     print()
+
+
 def time_metrics(**kwargs):
     start = kwargs["start"]
     end = kwargs["end"]
@@ -35,3 +59,5 @@ def time_metrics(**kwargs):
                   f" | Remaining time → {remaining_minutes} | ETA → {future_date_and_time_str}"
     print(colored(progress_str, "green"))
     print(colored(details_str, "magenta"))
+    print(print_progress_bar(current_index, size, prefix='Progress:', suffix='Complete', length=100))
+    print("")

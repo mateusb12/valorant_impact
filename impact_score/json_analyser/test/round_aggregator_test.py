973f74b41d0f944da5d5bd4ef2e0307c971a9eb1
mateusb12 <mateus_b09@hotmail.com>
2022-08-18 14:19:49 -0300

    Round aggregator test

--- impact_score/json_analyser/test/round_aggregator_test.py
+++ impact_score/json_analyser/test/round_aggregator_test.py
@@ -5,16 +5,91 @@ from impact_score.json_analyser.pool.analyser_pool import analyser_pool
 
 
 class RoundAggregatorTest(unittest.TestCase):
+    ar = None
+
     def setUp(self) -> None:
+        pass
+
+    @classmethod
+    def setUpClass(cls) -> None:
         a = analyser_pool.acquire()
         a.set_match(74099)
-        self.ar = AnalyserRound(a)
-        self.ar.pick_round(2)
+        cls.ar = AnalyserRound(a)
+        cls.ar.pick_round(2)
+        cls.pot = cls.ar.generate_full_round()
+        cls.index = 4
 
-    def test_generate_full_round(self):
-        actual = self.ar.generate_full_round()
-        expected = 1
-        self.assertEqual(actual, expected)  # add assertion here
+    def test_regular_time(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["RegularTime"]
+        expected = 64
+        self.assertEqual(actual, expected)
+
+    def test_spike_time(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["SpikeTime"]
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_final_winner(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["FinalWinner"]
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_round_time(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["RoundTime"]
+        expected = 36009
+        self.assertEqual(actual, expected)
+
+    def test_atk_loadout_value(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["ATK_loadoutValue"]
+        expected = 900
+        self.assertEqual(actual, expected)
+
+    def test_atk_shields(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["ATK_shields"]
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_atk_remaining_creds(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["ATK_remainingCreds"]
+        expected = 5850
+        self.assertEqual(actual, expected)
+
+    def test_atk_kills(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["ATK_kills"]
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_def_loadout_value(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["DEF_loadoutValue"]
+        expected = 9400
+        self.assertEqual(actual, expected)
+
+    def test_def_shields(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["DEF_shields"]
+        expected = 150
+        self.assertEqual(actual, expected)
+
+    def test_def_remaining_creds(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["DEF_remainingCreds"]
+        expected = 1700
+        self.assertEqual(actual, expected)
+
+    def test_def_kills(self):
+        gamestate = self.pot[self.index]
+        actual = gamestate["DEF_kills"]
+        expected = 2
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':

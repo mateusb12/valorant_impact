67c9acdd4affbf5002301cfcdf3beaa2f33f4416
mateusb12 <mateus_b09@hotmail.com>
2023-02-25 11:25:09 -0300

    colored scale bar

--- impact_score/model/model_improvement/shap_tests/kaggle_custom_plot.py
+++ impact_score/model/model_improvement/shap_tests/kaggle_custom_plot.py
@@ -2,6 +2,9 @@ from typing import List
 
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
+from matplotlib.figure import Figure
 
 
 def scatter_plot_probabilities(ax: plt.Axes, feature_names: List[str], feature_values: List,
@@ -17,7 +20,7 @@ def scatter_plot_probabilities(ax: plt.Axes, feature_names: List[str], feature_v
             new_y = i + 0.05
 
         # Plot the point
-        ax.scatter(new_x, new_y, color='blue')
+        ax.scatter(new_x, new_y, color='blue', zorder=4)
 
         if previous_x is not None and previous_y is not None:
             ax.plot([previous_x, new_x], [previous_y, new_y], color='gray', linestyle='--', linewidth=1)
@@ -47,7 +50,7 @@ def plot_bottom_axis(ax: plt.Axes, feature_names: List[str], probabilities: List
     return ax
 
 
-def plot_upper_axis(ax: plt.Axes) -> plt.Axes:
+def plot_upper_axis(ax: plt.Axes, fig: Figure) -> plt.Axes:
     ax2 = ax.twiny()
     x_ticks = ax.get_xticks()
     new_labels = [100 * (x + 0.5) for x in x_ticks]
@@ -57,7 +60,25 @@ def plot_upper_axis(ax: plt.Axes) -> plt.Axes:
     ax2.set_xlim(ax.get_xlim())
     ax2.set_ylim(ax.get_ylim())
     ax2.set_xlabel('Probability', fontsize=14)
+    ax2.xaxis.set_label_coords(0.5, 1.1)
+
+    plot_colored_scale_bar(fig)
     return ax2
+
+
+def plot_colored_scale_bar(fig: Figure):
+    cb_axes = fig.add_axes([0.125, 0.88, 0.7746, 0.015])
+    color_map = plt.cm.viridis
+    norm = Normalize(vmin=0, vmax=1)
+    scalar_map = ScalarMappable(norm=norm, cmap=color_map)
+    color_bar = fig.colorbar(scalar_map, cax=cb_axes, orientation='horizontal')
+    color_bar.set_ticklabels([])
+    color_bar.set_ticks([])
+    cb_axes.set_axisbelow(True)
+    cb_axes.set_zorder(1)
+    cb_axes.set_xticks([])
+    cb_axes.set_xticklabels([])
+    cb_axes.set_yticklabels([])
 
 
 def custom_decision_plot():
@@ -66,15 +87,16 @@ def custom_decision_plot():
     feature_names = [" ", "feature1", "feature2", "feature3", "feature4", "feature5"]
     fig, ax = plt.subplots(figsize=(10, 6))
 
-    scatter_plot_probabilities(ax, feature_names, feature_values, probabilities)
-
     ax = plot_bottom_axis(ax, feature_names, probabilities)
-    ax2 = plot_upper_axis(ax)
+    ax2 = plot_upper_axis(ax, fig)
+
+    scatter_plot_probabilities(ax, feature_names, feature_values, probabilities)
 
     plt.show()
 
 
 def __main():
+    # __hidden()
     custom_decision_plot()
 
 
